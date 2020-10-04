488df74585a236e133824d5e979c89be6ca0c5bb
madde1 <maddeswish@gmail.com>
2020-10-04 13:06:03 +0200

    add project!

---
+++ setting.js
@@ -0,0 +1,9 @@
+var mycanvas = document.getElementById('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 15;
+var w = 650;
+var h = 650;
+var score = 0;
+var snake;
+var snakeSize = 15;
+var food;
